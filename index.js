c16a5886e5c34a409766a08385c3e1cef41e0605
ramilatagiyeva <tagiyevaramila02@gmail.com>
2024-11-01 14:27:35 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+ // let obj = [
+   // {
+   //     name: "Ali",
+   //     surname: "Aliyev",
+    //    age: 10,
+   //     driving: true
+   // }, {
+   //     name: "Veli",
+    //    surname: "Aliyev",
+    //    age: 17,
+    //    driving: false,
+   // }, {
+   //     name: "Mehman",
+    //    surname: "Aliyev",
+    //    age: 25,
+    //    driving: true,
+   // },{
+   //     name: "Nicat",
+   //     surname: "Aliyev",
+     //   age:26,
+     //   driving: false
+   // },{
+     //   name: "Ayxan",
+      //  surname: "Aliyev",
+      //  age:15,
+       // driving: true
+   // }
+ // ]
+
+
+// bu arrayde driving (suruculuk vesiqesi) true olanlari ve yasi 18+ olanlari secib bir 
+// arrayde loga cixardacaqsiz 
+// soyadi Aliyev olanlari ve yasi 18 den asagi olanlari  secib bir arrayde geri qaytarin
+
+
+let obj = [
+    { name: "Ali", surname: "Aliyev", age: 10, driving: true },
+    { name: "Veli", surname: "Aliyev", age: 17, driving: false },
+    { name: "Mehman", surname: "Aliyev", age: 25, driving: true },
+    { name: "Nicat", surname: "Aliyev", age: 26, driving: false },
+    { name: "Ayxan", surname: "Aliyev", age: 15, driving: true }
+  ];
+  let drivingPlus = obj.filter(person => person.driving || person.age >= 18);
+  let aliyevUnder18 = obj.filter(person => person.surname === "Aliyev" && person.age < 18);
+  console.log(drivingPlus);
+  console.log(aliyevUnder18);
+
